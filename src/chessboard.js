a53e658d4b722f0b1f851c1f88180a53d566d8ec
lottenoorlander <lottenoorlander@gmail.com>
2020-03-27 21:32:43 +0100

    organised functions and created functions to draw the tiles

--- src/chessboard.js
+++ src/chessboard.js
@@ -1,21 +1,49 @@
 import "./chessboard.css";
 
 export default function chessboard(width, height) {
+  const canvas = drawCanvas(width, height);
+  drawBoard(canvas, width, height);
+  return canvas;
+}
+
+function drawCanvas(width, height) {
   const chessboard = document.createElement("canvas");
+  chessboard.classList.add("chessboard-container");
   chessboard.width = width * 100;
   chessboard.height = height * 100;
-  const filled = chessboard.getContext("2d");
+  return chessboard;
+}
 
-  for (let i = 0; i <= height; i += 2) {
-    for (let j = 0; j <= width; j += 2) {
-      filled.fillStyle = i % 2 === 0 ? "white" : "black";
-      filled.fillRect(j * 100, i * 100, 100, 100);
-      filled.fillStyle = i % 2 === 0 ? "black" : "white";
-      filled.fillRect((j + 1) * 100, (i + 1) * 100, 100, 100);
+function drawBoard(canvas, width, height) {
+  const chessboard = canvas.getContext("2d");
+
+  for (let i = 0; i <= height; i++) {
+    for (let j = 0; j <= width; j++) {
+      const color = getColor(i, j);
+      console.log(i, j, color);
+      drawTile(chessboard, 100, color, i, j);
     }
   }
-
-  chessboard.classList.add("chessboard-container");
-
   return chessboard;
+}
+
+function getColor(x, y) {
+  if (y % 2 === 0) {
+    if (x % 2 === 0) {
+      return "white";
+    } else {
+      return "black";
+    }
+  } else {
+    if (x % 2 === 0) {
+      return "black";
+    } else {
+      return "white";
+    }
+  }
+}
+
+function drawTile(board, width, color, x, y) {
+  board.fillStyle = color;
+  board.fillRect(y * width, x * width, width, width);
 }
